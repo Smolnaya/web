86544cd615b34341d3e13ca8b0f7feb9cee5025e
Smolnaya <a.smolnaja@ya.ru>
2020-03-31 22:34:31 +0300

    Добавлена проверка на сервере и кнопки перехода по базе пользователей

---
+++ src/main/java/com/web/app/api/request/UsersIdRequest.java
@@ -0,0 +1,22 @@
+package com.web.app.api.request;
+
+import java.util.ArrayList;
+import java.util.List;
+/*
+Список всех ID в БД
+ */
+public class UsersIdRequest {
+    private List<Integer> listId;
+
+    public UsersIdRequest() {
+        this.listId = new ArrayList<>();
+    }
+
+    public List<Integer> getListId() {
+        return listId;
+    }
+
+    public void setListId(List<Integer> listId) {
+        this.listId = listId;
+    }
+}
